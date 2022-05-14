f53cee7330c1bff76e162284088730700bc0a540
Femi <47154698+oojo12@users.noreply.github.com>
2022-05-14 15:01:14 -0400

    send message to webserver

--- notification_system/docker/app/app.py
+++ notification_system/docker/app/app.py
@@ -1,2 +1,19 @@
-import time
-print("HI THERE")
+import os
+import random
+import requests
+
+WEBSEVER_URL = os.environ['WEBSERVER_URL']
+LOG_TYPES = ['info', 'warning', 'error']
+
+messages_sent = 0
+while True:
+    message = {
+        'From': "a@example.com",
+        "To": "b@example.com",
+        "Subject": f"{LOG_TYPES[random.randint(len(LOG_TYPES)]}",
+        "Message-ID": f"<ant> {LOG_TYPES[random.randint(len(LOG_TYPES)]}"
+    }
+    requests.post(WEBSERVER_URL, message)
+    messages_sent += 1
+    if messages == 10:
+        break
