9e8c94e32c6e0a04b2d4349a212f85268cb61d9b
Femi <olufemi@grid.ai>
2022-05-13 20:55:38 -0400

    complete example

--- notification_server/example.py
+++ notification_server/example.py
@@ -1,20 +1,38 @@
-import smtplib
+import asyncio
+from smtplib import SMTP as Client
+from aiosmtpd.controller import Controller
 
-sender = 'from@fromdomain.com'
-receivers = ['to@todomain.com']
+class ExampleHandler:
+ async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
+     if not address.endswith('@example.com'):
+         return '550 not relaying to that domain'
+     envelope.rcpt_tos.append(address)
+     return '250 OK'
 
-message = """From: From Person <from@fromdomain.com>
-To: To Person <to@todomain.com>
-Subject: SMTP e-mail test
+ async def handle_DATA(self, server, session, envelope):
+     print('Message from %s' % envelope.mail_from)
+     print('Message for %s' % envelope.rcpt_tos)
+     print('Message data:\n')
+     for ln in envelope.content.decode('utf8', errors='replace').splitlines():
+         print(f' {ln}'.strip())
+     print()
+     print('End of message')
+     return '250 Message accepted for delivery'
 
-This is a test e-mail message.
-"""
 
-smtpObj = smtplib.SMTP('localhost')
+controller = Controller(ExampleHandler())
+controller.start()
 
-try:
-   smtpObj = smtplib.SMTP('localhost')
-   smtpObj.sendmail(sender, receivers, message)
-   print "Successfully sent email"
-except SMTPException:
-   print "Error: unable to send email"
+
+client = Client(controller.hostname, controller.port)
+r = client.sendmail('a@example.com', ['b@example.com'], """\
+ From: Anne Person <anne@example.com>
+ To: Bart Person <bart@example.com>
+ Subject: A test
+ Message-ID: <ant>
+
+ Hi Bart, this is Anne.
+ """)
+
+
+controller.stop()
