89c57d8a55e31c9ce46234ccd3e6aa76e9374241
Femi <47154698+oojo12@users.noreply.github.com>
2022-05-14 15:05:38 -0400

    fix post url

--- notification_system/docker/app/app.py
+++ notification_system/docker/app/app.py
@@ -7,13 +7,14 @@ LOG_TYPES = ['info', 'warning', 'error']
 
 messages_sent = 0
 while True:
+    log_type = LOG_TYPES[random.randint(len(LOG_TYPES))
     message = {
         'From': "a@example.com",
         "To": "b@example.com",
-        "Subject": f"{LOG_TYPES[random.randint(len(LOG_TYPES)]}",
-        "Message-ID": f"<ant> {LOG_TYPES[random.randint(len(LOG_TYPES)]}"
+        "Subject": f"{log_type}",
+        "Message-ID": f"<ant> {log_type}"
     }
-    requests.post(WEBSERVER_URL, message)
+    requests.post(WEBSERVER_URL + f"/{log_type}", message)
     messages_sent += 1
     if messages_sent == 10:
         break
